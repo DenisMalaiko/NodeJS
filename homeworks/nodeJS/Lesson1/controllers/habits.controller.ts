d3af773a18213ab39085173c008c3c4004423dc9
denysmalaiko <malaiko.denis@gmail.com>
2025-06-15 14:28:39 +0200

    Improvements

--- homeworks/nodeJS/Lesson1/controllers/habits.controller.ts
+++ homeworks/nodeJS/Lesson1/controllers/habits.controller.ts
@@ -4,18 +4,34 @@ export class HabitsController {
   constructor() {}
 
   async add(name: string, freq: string) {
-     return await service.addHabit(name, freq)
+    if (!name || !freq) {
+      console.error('Use: npm run habit -- add --name "<текст звички>" --freq <daily|weekly|monthly>');
+      return;
+    }
+    return await service.addHabit(name, freq)
   }
 
   async update(id: string, name: string, freq: string) {
+    if (!id || !name || !freq) {
+      console.error('Use: npm run habit -- update --id <ідентифікатор> --name "<текст звички>" --freq <daily|weekly|monthly>');
+      return;
+    }
     return await service.updateHabit(id, name, freq);
   }
 
   async done(id: string) {
+    if (!id) {
+      console.error('Use: npm run habit -- done --id <ідентифікатор>');
+      return;
+    }
     return await service.doneHabit(id);
   }
 
   async delete(id: string) {
+    if (!id) {
+      console.error('Use: npm run habit -- delete --id <ідентифікатор>');
+      return;
+    }
     return await service.deleteHabit(id);
   }
 
--- homeworks/nodeJS/Lesson1/eslint.config.js
+++ homeworks/nodeJS/Lesson1/eslint.config.js
@@ -1,11 +1,22 @@
-import js from "@eslint/js";
-import globals from "globals";
-import tseslint from "typescript-eslint";
-import { defineConfig } from "eslint/config";
-
+import { defineConfig } from 'eslint/config';
+import js from '@eslint/js';
+import globals from 'globals';
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], languageOptions: { globals: globals.browser } },
-  tseslint.configs.recommended,
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 2022,
+      globals: {
+        ...globals.node,
+      },
+    },
+    plugins: { js },
+    extends: ['js/recommended'],
+    rules: {
+      semi: ['error', 'always'],
+      quotes: ['error', 'single'],
+      'no-console': 'warn',
+    },
+  },
 ]);
