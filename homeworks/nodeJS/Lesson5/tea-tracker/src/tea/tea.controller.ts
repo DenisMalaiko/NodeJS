6ea75bbc8bc156e3b5d1d659713949e3a02c894d
denysmalaiko <malaiko.denis@gmail.com>
2025-06-30 16:41:48 +0200

    Lesson 5 | Improve Tea Controller and Service

--- homeworks/nodeJS/Lesson5/tea-tracker/src/tea/tea.controller.ts
+++ homeworks/nodeJS/Lesson5/tea-tracker/src/tea/tea.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
+import { TeaService } from './tea.service';
+import { Tea } from '../../entities/tea.entity';
 
 @Controller('tea')
-export class TeaController {}
+export class TeaController {
+  constructor(private readonly teas: TeaService) {}
+  @Get()
+  getAll(@Query('minRating') minRating: string): Tea[] {
+    return this.teas.getAll(minRating);
+  }
+  @Get(':id')
+  getById(@Param('id') id: string): Tea {
+    return this.teas.getById(id);
+  }
+}
