67d7e78d9d3647735c0b80aed3a21bdae0ce7fb8
denysmalaiko <malaiko.denis@gmail.com>
2025-07-02 18:39:04 +0200

    Lesson 5 | Add Dockerfile

--- homeworks/nodeJS/Lesson5/tea-tracker/src/common/guards/api-key.guard.ts
+++ homeworks/nodeJS/Lesson5/tea-tracker/src/common/guards/api-key.guard.ts
@@ -12,21 +12,15 @@ export class ApiKeyGuard implements CanActivate {
   constructor(private reflector: Reflector) {}
 
   canActivate(context: ExecutionContext): boolean {
-    console.log("IS_PUBLIC_KEY ", IS_PUBLIC_KEY)
-
     const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
       context.getHandler(),
       context.getClass(),
     ]);
 
-    console.log("Is Public ", isPublic)
-
     if (isPublic) return true;
 
     const request = context.switchToHttp().getRequest();
     const apiKey = request.headers['x-api-key'];
-
-    console.log("API KEY ", apiKey)
 
     if (apiKey !== 'TEST') {
       throw new ForbiddenException('Invalid API key');
