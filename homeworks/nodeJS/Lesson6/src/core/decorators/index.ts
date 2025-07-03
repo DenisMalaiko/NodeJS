2ebf4146dadd21427f464a08a460eb772fad6e5c
denysmalaiko <malaiko.denis@gmail.com>
2025-07-03 19:45:01 +0200

    Lesson 6 | Init

---
+++ homeworks/nodeJS/Lesson6/src/core/decorators/index.ts
@@ -0,0 +1,6 @@
+export * from './module';
+export * from './injectable';
+export * from './controller';
+export * from './params';
+export * from './route';
+export * from './use-pipes';
---
+++ homeworks/nodeJS/Lesson6/src/index.ts
@@ -0,0 +1,13 @@
+import "reflect-metadata"
+import {BooksModule} from "./apps/books/books.module";
+import {Factory} from "./core/http";
+
+process.on('uncaughtException', (err) => {
+  console.error('Uncaught Exception:', err);
+});
+
+const app = Factory([BooksModule])
+
+const port = 8081;
+
+app.listen(port, () => console.log(`Mini-Nest listening on http://localhost:${port}`));
